7ba53efd046812c9441a06d9018a0d8079e5eb68
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-06-07 19:15:19 +0300

    add user

--- controllers/answerController.js
+++ controllers/answerController.js
@@ -1,103 +1,103 @@
-const { Answer, Task, User } = require('../models');
 
-// Add a new answer
-const addAnswer = async (req, res) => {
-    try {
-        const { text, task_id, author_id } = req.body;
 
-        const taskExists = await Task.findById(task_id);
-        if (!taskExists) {
-            return res.status(404).json({ message: 'Task not found' });
+const Task = require("../models/Task");
+const User = require("../models/User");
+const Answer = require("../models/Answer");
+
+class AnswerController {
+    async addAnswer  (req, res) {
+        try {
+            const { text, task_id, author_id } = req.body;
+    
+            const taskExists = await Task.findById(task_id);
+            if (!taskExists) {
+                return res.status(404).json({ message: 'Task not found' });
+            }
+    
+            const authorExists = await User.findById(author_id);
+            if (!authorExists) {
+                return res.status(404).json({ message: 'Author not found' });
+            }
+    
+            const newAnswer = new Answer({ text, task_id, author_id });
+            await newAnswer.save();
+    
+            res.status(201).json(newAnswer);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
         }
-
-        const authorExists = await User.findById(author_id);
-        if (!authorExists) {
-            return res.status(404).json({ message: 'Author not found' });
+    };
+    
+    // Delete an answer
+    async deleteAnswer  (req, res)  {
+        try {
+            const { id } = req.params;
+    
+            const answerToDelete = await Answer.findById(id);
+            if (!answerToDelete) {
+                return res.status(404).json({ message: 'Answer not found' });
+            }
+    
+            await answerToDelete.remove();
+    
+            res.status(200).json({ message: 'Answer deleted successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
         }
-
-        const newAnswer = new Answer({ text, task_id, author_id });
-        await newAnswer.save();
-
-        res.status(201).json(newAnswer);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// Delete an answer
-const deleteAnswer = async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        const answerToDelete = await Answer.findById(id);
-        if (!answerToDelete) {
-            return res.status(404).json({ message: 'Answer not found' });
+    };
+    
+    // Update an answer
+    async updateAnswer  (req, res) {
+        try {
+            const { id } = req.params;
+            const { text, task_id, author_id } = req.body;
+    
+            const answerToUpdate = await Answer.findById(id);
+            if (!answerToUpdate) {
+                return res.status(404).json({ message: 'Answer not found' });
+            }
+    
+            answerToUpdate.text = text;
+            answerToUpdate.task_id = task_id;
+            answerToUpdate.author_id = author_id;
+            await answerToUpdate.save();
+    
+            res.status(200).json(answerToUpdate);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
         }
-
-        await answerToDelete.remove();
-
-        res.status(200).json({ message: 'Answer deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// Update an answer
-const updateAnswer = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { text, task_id, author_id } = req.body;
-
-        const answerToUpdate = await Answer.findById(id);
-        if (!answerToUpdate) {
-            return res.status(404).json({ message: 'Answer not found' });
+    };
+    
+    // Get an answer by ID
+    async getAnswerById  (req, res) {
+        try {
+            const { id } = req.params;
+    
+            const answerDetails = await Answer.findById(id).populate('task_id').populate('author_id', 'first_name last_name email');
+    
+            if (!answerDetails) {
+                return res.status(404).json({ message: 'Answer not found' });
+            }
+    
+            res.status(200).json(answerDetails);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
         }
-
-        answerToUpdate.text = text;
-        answerToUpdate.task_id = task_id;
-        answerToUpdate.author_id = author_id;
-        await answerToUpdate.save();
-
-        res.status(200).json(answerToUpdate);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// Get an answer by ID
-const getAnswerById = async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        const answerDetails = await Answer.findById(id).populate('task_id').populate('author_id', 'first_name last_name email');
-
-        if (!answerDetails) {
-            return res.status(404).json({ message: 'Answer not found' });
+    };
+    
+    // Get all answers for a task
+    async getAnswersByTaskId  (req, res) {
+        try {
+            const { task_id } = req.params;
+    
+            const answers = await Answer.find({ task_id }).populate('author_id', 'first_name last_name email');
+    
+            res.status(200).json(answers);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
         }
+    };
+    
+}
 
-        res.status(200).json(answerDetails);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// Get all answers for a task
-const getAnswersByTaskId = async (req, res) => {
-    try {
-        const { task_id } = req.params;
-
-        const answers = await Answer.find({ task_id }).populate('author_id', 'first_name last_name email');
-
-        res.status(200).json(answers);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-module.exports = {
-    addAnswer,
-    deleteAnswer,
-    updateAnswer,
-    getAnswerById,
-    getAnswersByTaskId
-};
+module.exports = new AnswerController
--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,98 +1,96 @@
-const { Task, Class } = require('../models');
 
+const Course = require("../models/Course");
+const Task = require("../models/Task");
 
-const addTask = async (req, res) => {
-    try {
-        const { name, created_at, text, class_id, is_done } = req.body;
-
-        const classExists = await Class.findById(class_id);
-        if (!classExists) {
-            return res.status(404).json({ message: 'Class not found' });
+class TaskController {
+    async addTask (req, res) {
+        try {
+            const { name, created_at, text, course_id, is_done } = req.body;
+    
+            const classExists = await Course.findById(course_id);
+            if (!classExists) {
+                return res.status(404).json({ message: 'Class not found' });
+            }
+    
+            const newTask = new Task({ name, created_at, text, course_id, is_done });
+            await newTask.save();
+    
+            res.status(201).json(newTask);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
         }
-
-        const newTask = new Task({ name, created_at, text, class_id, is_done });
-        await newTask.save();
-
-        res.status(201).json(newTask);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// Delete a task
-const deleteTask = async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        const taskToDelete = await Task.findById(id);
-        if (!taskToDelete) {
-            return res.status(404).json({ message: 'Task not found' });
+    };
+    
+    // Delete a task
+    async deleteTask  (req, res)  {
+        try {
+            const { id } = req.params;
+    
+            const taskToDelete = await Task.findById(id);
+            if (!taskToDelete) {
+                return res.status(404).json({ message: 'Task not found' });
+            }
+    
+            await taskToDelete.remove();
+    
+            res.status(200).json({ message: 'Task deleted successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
         }
-
-        await taskToDelete.remove();
-
-        res.status(200).json({ message: 'Task deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// Update a task
-const updateTask = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { name, created_at, text, class_id, is_done } = req.body;
-
-        const taskToUpdate = await Task.findById(id);
-        if (!taskToUpdate) {
-            return res.status(404).json({ message: 'Task not found' });
+    };
+    
+    // Update a task
+    async updateTask  (req, res) {
+        try {
+            const { id } = req.params;
+            const { name, created_at, text, course_id, is_done } = req.body;
+    
+            const taskToUpdate = await Task.findById(id);
+            if (!taskToUpdate) {
+                return res.status(404).json({ message: 'Task not found' });
+            }
+    
+            taskToUpdate.name = name;
+            taskToUpdate.created_at = created_at;
+            taskToUpdate.text = text;
+            taskToUpdate.course_id = course_id;
+            taskToUpdate.is_done = is_done;
+            await taskToUpdate.save();
+    
+            res.status(200).json(taskToUpdate);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
         }
-
-        taskToUpdate.name = name;
-        taskToUpdate.created_at = created_at;
-        taskToUpdate.text = text;
-        taskToUpdate.class_id = class_id;
-        taskToUpdate.is_done = is_done;
-        await taskToUpdate.save();
-
-        res.status(200).json(taskToUpdate);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// Get a task by ID
-const getTaskById = async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        const taskDetails = await Task.findById(id).populate('class_id');
-
-        if (!taskDetails) {
-            return res.status(404).json({ message: 'Task not found' });
+    };
+    
+    // Get a task by ID
+    async getTaskById (req, res) {
+        try {
+            const { id } = req.params;
+    
+            const taskDetails = await Task.findById(id).populate('class_id');
+    
+            if (!taskDetails) {
+                return res.status(404).json({ message: 'Task not found' });
+            }
+    
+            res.status(200).json(taskDetails);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
         }
+    };
+    
+    
+    async getAllTasks(req, res) {
+        try {
+            const tasks = await Task.find().populate('class_id');
+    
+            res.status(200).json(tasks);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    };
+    
+}
 
-        res.status(200).json(taskDetails);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-
-const getAllTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find().populate('class_id');
-
-        res.status(200).json(tasks);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-module.exports = {
-    addTask,
-    deleteTask,
-    updateTask,
-    getTaskById,
-    getAllTasks
-};
+module.exports = new TaskController
