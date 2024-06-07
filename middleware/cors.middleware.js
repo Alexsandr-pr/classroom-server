8bab7e167f3bc8a26811c0ce32be31a0684689d8
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-06-07 19:29:21 +0300

    add

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,8 +1,9 @@
 
+require("dotenv").config()
 
 
 function cors(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
